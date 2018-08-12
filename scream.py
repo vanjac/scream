d606a4c8d2a0941ba0b35fa4a29f6df45f3c68d4
Jacob van't Hoog <vantjac@gmail.com>
2018-08-11 20:50:55 -0400

    Fix key codes for termux

--- scream.py
+++ scream.py
@@ -8,9 +8,9 @@ def main(filename):
         screen.addstr(f.read())
         while True:
             key = screen.getch()
-            if key == 3: # ctrl-c
+            if key == 17: # ctrl-q
                 break
-            if key == 8: # backspace
+            if key == 8 or key == 127: # backspace
                 f.seek(f.tell() - 1, io.SEEK_SET) # move back one character
                 f.truncate(f.tell()) # delete character from file
                 screen.delch()
